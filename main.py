2bd563992944fd4d689b6ea8c82ad8db41adcaa9
Moritz Konstantin Schlegelmilch <moritzk.schlegelmilch@gmail.com>
2026-04-15 15:31:19 +0200

    Revert "[Subprocesses] Implements subprocesses"
    This reverts commit 8b3209bd83f5c49baf915aebe39120105d7e0fc9.

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ discovery = ProcessAreaDiscovery(ocel)
 discovery.run()
 discovery.visualize()
 
-#test_log_folder("./example_simulation_data", "./testing_output", recursive=True)
+#print(test_log_folder("./example_simulation_data", "./testing_output", recursive=True))
